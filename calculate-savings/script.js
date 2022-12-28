d1ca12e34b32eab82970d52a3a667d2348f8d119
dielelisa <121568213+dielelisa@users.noreply.github.com>
2022-12-28 18:39:43 +0100

    Update script.js

--- calculate-savings/script.js
+++ calculate-savings/script.js
@@ -10,11 +10,9 @@ function percentage() {
 	
 	//Calculate the percentages
 	var homeSavings = (num / 100) * percentHome;
-	var vacationSavings = (num / 100) * percentHome;
+	var vacationSavings = (num / 100) * percentVacation;
 
 	// Show values
-	document.getElementById('amounts').innerHTML = "Home (12%): €" + homeSavings + "</br>" + "Vacation (8%): €" + vacationSavings;
+	document.getElementById('amounts').innerHTML = "Home (12%): €" + homeSavings + "<br>" + "Vacation (8%): €" + vacationSavings;
 
-	//document.getElementById("amounts").value = 
-	//	"Home (12%): " + homeSavings + "<br>" + "Vacation (8%): " + vacationSavings ;
 }
